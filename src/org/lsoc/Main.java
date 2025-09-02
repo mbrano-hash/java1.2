70c01938e5a4634ab758d63a40d89bb0c47e9869
szebrack-LatinChicago <szebrack@latinschool.org>
2025-09-02 14:15:52 +0000

    Set up instructions

--- src/org/lsoc/Main.java
+++ src/org/lsoc/Main.java
@@ -1,7 +1,7 @@
 public class Main {// PRESS OPTION + SHIFT + F to read the file
     public static void main(String[] args) {
 
-        System.out.println("This is the main file, the homework is in another file! Keep looking!!!");
+        System.out.println("Nothing useful here.");
 
     }
 
