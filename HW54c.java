f410f6424569fa9a6645a37c9dd02b0cd5fc5032
mbrano-hash <mbranova@lsoc.org>
2025-09-04 17:28:52 +0000

    Finished HW54c

---
+++ HW54c.java
@@ -0,0 +1,11 @@
+public class HW54c {
+  public static void main(String [] args){
+    double pi = 3.1415926;
+    double radius = 3.712;
+    double area = pi * radius * radius; 
+    double circum = 2 * pi * radius; 
+    System.out.println(area);
+    System.out.println(circum);
+
+  }
+}
