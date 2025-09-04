c735030c44cb25c077591cd259769ff191bd3afb
mbrano-hash <mbranova@lsoc.org>
2025-09-04 17:42:26 +0000

    Wrote HW54A

---
+++ HW54a.java
@@ -0,0 +1,19 @@
+public class HW54a{
+  public static void main(String [] asgr){
+    int bugmiles = 286;
+    int buggallons = 9;
+    int birdmiles = 412;
+    int birdgallons = 40; 
+    int submiles = 361;
+    int subgallons = 18;
+    int cutlassmiles = 286;
+    int cutgallons = 9;
+    int totalmiles = birdmiles + bugmiles + submiles + cutlassmiles;
+    int totalgallons = birdgallons + buggallons + subgallons + cutgallons;
+    System.out.println(totalmiles); 
+    System.out.println(totalgallons);
+
+
+    
+  }
+}
