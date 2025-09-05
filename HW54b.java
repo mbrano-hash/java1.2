0307a5b07cfe169ee1bbe7452d0b57f0f714c338
mbrano-hash <mbranova@lsoc.org>
2025-09-05 02:53:42 +0000

    Wrote 54b

---
+++ HW54b.java
@@ -0,0 +1,15 @@
+public class HW54b{
+  public static void main(String [] asgr){
+    int frst = 475;
+    int scnd = 821;
+    int thrd = 369;
+    int frth = 526;
+    int sum = frst + scnd + thrd + frth;
+    int total = sum + frst + scnd + thrd + frth;
+    double avg = total / 5.0;
+    System.out.println(avg);
+
+
+
+  }
+}
